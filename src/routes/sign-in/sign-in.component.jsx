cd14016ddc0a9d058fe8863ee9194d65391f7dd1
NameisOmkaar <Kotojuomkar@gmail.com>
2022-12-15 17:52:27 +0530

    "created sign up component, the form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -8,6 +8,7 @@ const SignIn = () => {
   const logGoogleuser = async () => {
     const { user } = await signInWithGooglePopup();
      const userDocRef = await createUserDocumentFromAuth(user);
+     console.log(userDocRef)
   };
   return (
     <div>
