5eb1299ff88dea8bd4e7dec04f7095a3ce004365
NameisOmkaar <Kotojuomkar@gmail.com>
2022-12-21 13:05:17 +0530

    added onAuthChange element

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,13 +1,28 @@
-import {createContext, useState} from 'react';
+import { createContext, useState, useEffect } from "react";
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
-currentUser: null,
-setCurrentUser: () => null
+  currentUser: null,
+  setCurrentUser: () => null,
 });
 
-export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = {currentUser, setCurrentUser}
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
 
-return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+  useEffect(() => {
+    const unSubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+      console.log(user);
+    });
+    return unSubscribe;
+  }, []);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
